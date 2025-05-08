6ea0e96fa64494cb769580557e4d9620459a81fa
Shamilseferov <shamilseferov@gmail.com>
2025-05-08 15:39:45 +0400

    Real estate v1.5

--- js/data.js
+++ js/data.js
@@ -71,15 +71,18 @@ const listings = [
 
   {
     id: 6,
-    title: "2 otaqlı mənzil - Alov Qüllələri",
-    type: "kirayə",
-    price: "20,000 AZN",
-    location: "Bakı, Səbail",
-    district: "Nizami",
-    area: 150, // kvadrat metr
+    title: "2 otaqlı mənzil - MİDA Yaşayış Kompleksi",
+    type: "Satılır",
+    price: "155000 AZN",
+    location: "Bakı, Yasamal",
+    district: "Yasamal",
+    area: 65, // kvadrat metr
     rooms: 2,
     status: "new",
-    image: ["images/Samilin-evi/ev1.jpg", "images/ev1-2.jpg", "images/ev1-3.jpg"],
-    description: "Möhtəşəm şəhər mənzərəsi ilə rahat mənzil.",
+    image: ["images/9-6-2 155000/1.jpeg", "images/9-6-2 155000/2.jpg", "images/ev1-3.jpg"],
+    description: "Satılır 2 otaqlı yeni tikili 65 m², Yasamal q." + "<br> </br> "+ "Mida Yasamal yaşayış kompleksi Zefir Mall, 161 və 133 nömrəli məktəbin yanında 9 Mərtəbəli binanın 6-cımərtəbəsində 65kv olan 2 otaq təmirli mənzil satılır. Qanuni 2 otaqdır. Çıxarış (Kupça), texpasport var. Sənədləri qaydasındadır. Göl mənzərəli mənzil. Kombi sistemi, Qaz, su, işıq, lift və internet daim var. Binanın geniş həyəti, parkı, besetkası, pulsuz stadionu və maşın saxlamağa yeri var. Kamera muhafizəsi var, komendat nəzarət edir. Həyət, blok hər gün təmizlənir. 7B, 9 və 10 nömrəli avtobuslar binanın yanından keçir. 155min manat. Ofisin xidmət haqqı 1 %",
+  
   },
+
+ 
 ];
