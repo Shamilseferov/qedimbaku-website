83f71696b46ec8661e4efcc8cb9bd92ba7fd86e4
Shamilseferov <shamilseferov@gmail.com>
2025-05-12 09:50:07 +0400

    Real estate v1.5

--- js/data.js
+++ js/data.js
@@ -1,76 +1,7 @@
 const listings = [
-  {
+    
+    {
     id: 1,
-    title: "3 otaqlı mənzil - Alov Qüllələri",
-    type: "satış",
-    price: "450,000 AZN",
-    location: "Bakı, Səbail",
-    district: "Yasamal",
-    area: 120, // kvadrat metr
-    rooms: 3,
-    status: "new", // "new" və ya "old"
-    image: ["images/ev2.jpg", "images/ev1-2.jpg", "images/ev1-3.jpg"],
-    description: "Möhtəşəm şəhər mənzərəsi ilə rahat mənzil.",
-  },
-  
-  {
-    id: 2,
-    title: "2 otaqlı mənzil - Alov Qüllələri",
-    type: "satış",
-    price: "320,000 AZN",
-    location: "Bakı, Səbail",
-    district: "Səbail",
-    area: 120, // kvadrat metr
-    rooms: 3,
-    status: "new",
-    image: ["images/ev2.jpg", "images/ev1-2.jpg", "images/ev1-3.jpg"],
-    description: "Möhtəşəm şəhər mənzərəsi ilə rahat mənzil.",
-  },
-
-  {
-    id: 3,
-    title: "2 otaqlı mənzil - Alov Qüllələri",
-    type: "satış",
-    price: "320,000 AZN",
-    location: "Bakı, Səbail",
-    district: "Səbail",
-    area: 120, // kvadrat metr
-    rooms: 3,
-    status: "new",
-    image: ["images/ev2.jpg", "images/ev1-2.jpg", "images/ev1-3.jpg"],
-    description: "Möhtəşəm şəhər mənzərəsi ilə rahat mənzil.",
-  },
-
-  {
-    id: 4,
-    title: "2 otaqlı mənzil - Alov Qüllələri",
-    type: "kirayə",
-    price: "320,000 AZN",
-    location: "Bakı, Səbail",
-    district: "Səbail",
-    area: 90, // kvadrat metr
-    rooms: 3,
-    status: "new",
-    image: ["images/ev2.jpg", "images/ev1-2.jpg", "images/ev1-3.jpg"],
-    description: "Möhtəşəm şəhər mənzərəsi ilə rahat mənzil.",
-  },
-
-  {
-    id: 5,
-    title: "2 otaqlı mənzil - Alov Qüllələri",
-    type: "kirayə",
-    price: "320,000 AZN",
-    location: "Bakı, Səbail",
-    district: "Səbail",
-    area: 120, // kvadrat metr
-    rooms: 5,
-    status: "old",
-    image: ["images/ev2.jpg", "images/ev1-2.jpg", "images/ev1-3.jpg"],
-    description: "Möhtəşəm şəhər mənzərəsi ilə rahat mənzil.",
-  },
-
-  {
-    id: 6,
     title: "2 otaqlı mənzil - MİDA Yaşayış Kompleksi",
     type: "satış",
     price: "155000 AZN",
@@ -86,7 +17,7 @@ const listings = [
 
 
   {
-    id: 7,
+    id: 2,
     title: "3 otaqlı mənzil - MİDA Yaşayış Kompleksi",
     type: "satış",
     price: "200000 AZN",
