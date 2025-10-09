6de671a12d6c546b9fc76573fee14c088c89bec5
Shamilseferov <shamilseferov@gmail.com>
2025-10-09 17:03:01 +0400

    r

--- js/data.js
+++ js/data.js
@@ -773,4 +773,21 @@ const listings = [
     description: "Mida Yasamal yaşayış kompleksi Zefir Mall, 161 və 133 nömrəli məktəbin yanında 9 Mərtəbəli binanin 8-ci mərtəbəsində 65kv olan 2 otaq təmirli mənzil satılır. Qanuni 2 otaqdır. Çıxarış (Kupça), texpasport var. Sənədləri qaydasındadır. Kombi sistemi, Qaz, su, işıq, lift və internet daim var. Binanın geniş həyəti, parkı, besetkası, pulsuz stadionu və maşın saxlamağa yeri var. Kamera muhafizəsi var, komendat nəzarət edir. Həyət, blok hər gün təmizlənir. 7B, 9 və 10 nömrəli avtobuslar binanın yanından keçir. Qiymət 170min manat. Ofisin xidmət haqqı 1 % təşkil edir",
   
   },
+
+   {
+    id: 53,
+    title: "3 otaqlı mənzil - Mida Yasamal",
+    type: "satış",
+    price: "200000 AZN",
+    location: "Bakı, Yasamal",
+    district: "Yasamal",
+    area: 85, // kvadrat metr
+    rooms: 3,
+    status: "new",
+    image: ["images/12-4-3-200000/1.jpeg", "images/12-4-3-200000/2.jpeg", "images/12-4-3-200000/3.jpeg", "images/12-4-3-200000/4.jpeg", "images/12-4-3-200000/5.jpeg", "images/12-4-3-200000/6.jpeg", "images/12-4-3-200000/7.jpeg", "images/12-4-3-200000/8.jpeg", "images/12-4-3-200000/9.jpeg"],
+    description: "Satılır 3 otaqlı yeni tikili 85 m², Yasamal q." + "<br> </br> "+ "Yasamal rayonu, MİDA Yasamal Yaşayış Kompleksində, Zefir Mall ticarət mərkəzi, 161 və 133 nömrəli məktəblərin yaxınlığında yerləşən 12 mərtəbəli binanın 4-cü mərtəbəsində 85 kvadrat metr sahəyə malik, qanuni 3 otaqlı, əla təmirli mənzil satılır. Mənzil qaz, su, işıq, lift və internetlə daimi təmin olunub. Sənədləri tam qaydasındadır. Bina geniş və səliqəli həyətə, park sahəsinə, besetkaya, pulsuz stadion və avtomobil saxlamaq üçün dayanacaq yerinə malikdir. Ərazi 24/7 kamera müşahidəsi altındadır və komendant tərəfindən nəzarət olunur. Blok və həyət hər gün mütəmadi şəkildə təmizlənir. Nəqliyyat baxımından əlverişli yerdə yerləşir – 7B, 9 və 10 nömrəli avtobus xətləri binanın yaxınlığından keçir. Qiymət 200min manat. Ofisin xidmət haqqı 1 %",
+  
+  },
+
+
 ];
